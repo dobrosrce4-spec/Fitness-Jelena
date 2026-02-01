8f3896e5bb196588357eae9c3444e64c0d865962
dobrosrce4-spec <dobrosrce4@gmail.com>
2026-02-01 20:00:55 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Fitness Jelena</h1>
+      <p>Ajmo jakoo 🔥</p>
+      <p>Aplikacija radi ✅</p>
+    </div>
+  );
+}
